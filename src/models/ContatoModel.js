3d118f8e5c2ea494a7535c0947abab7d7a18a295
ramonvm18 <ramon.mastropaulo@hotmail.com>
2021-07-23 18:06:15 -0300

    listando e apagando contatos

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -19,11 +19,6 @@ function Contato(body) {
     this.contato = null;
 }
 
-Contato.buscaPorId = async function(id) {
-  if(typeof id !== 'string') return;
-  const user = await ContatoModel.findById(id)
-  return user;
-}
 
 Contato.prototype.register = async function() {
     this.valida();
@@ -66,5 +61,26 @@ Contato.prototype.edit = async function(id) {
   if(this.errors.length > 0) return;
   this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true});
 }
+
+
+//METODOS ESTATICOS
+Contato.buscaPorId = async function(id) {
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findById(id)
+  return contato;
+}
+
+Contato.buscaContatos = async function() {
+  const contatos = await ContatoModel.find()
+    .sort({ criadoEm: -1 })
+  return contatos;
+}
+
+Contato.delete = async function(id) {
+  if(typeof id !== 'string') return
+  const contato = await ContatoModel.findOneAndDelete({ _id: id})   
+  return contato;
+}
+
 
 module.exports = Contato;
